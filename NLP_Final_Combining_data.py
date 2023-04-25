6ef65633ac0698ff223f7e36e52815e80e6c22f8
MeghanaGantla <meghana_gantla@gwu.edu>
2023-04-25 12:25:14 -0400

    combining data

---
+++ NLP_Final_Combining_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+path = os.path.join(os.path.dirname(__file__), 'Data')
+fake_path = os.path.join(path, 'Fake.csv')
+true_path = os.path.join(path, 'True.csv')
+data_path = os.path.join(path, 'Full_data.csv')
+
+# Read the input CSV files into pandas dataframes
+fake_df = pd.read_csv(fake_path)
+true_df = pd.read_csv(true_path)
+
+# Create a target variable
+fake_df['target'] = 0
+true_df['target'] = 1
+
+# Combine the dataframes into a single dataframe
+combined_df = pd.concat([fake_df, true_df])
+
+# Display the new Dataset
+print(combined_df.head())
+print('Dataframe columns: ', combined_df.columns)
+
+# Write the combined dataframe to a new CSV file
+combined_df.to_csv(data_path, index=False)
+
+print("Combined CSV file saved successfully at", data_path)
